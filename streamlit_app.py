95fed96a9316d0bfbfd90f5e362bfbc5b887a664
Zareen Rahman <155265507+zareenrahman@users.noreply.github.com>
2025-10-29 21:25:50 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+# =========================
+# file: streamlit_app.py   <-- PLACE THIS AT REPO ROOT
+# =========================
+from dashboard.app import main
+
+if __name__ == "__main__":
+    main()
